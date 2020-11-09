5559f6c6cf64060674272c91102a49d3905453af
Daniel Yankiver <dyankiver@gmail.com>
2020-11-09 20:29:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,9 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     binding.pry
+    value.each do 
+    
+    end 
     final_array
   end 
   
